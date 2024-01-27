97e3800769812c1675768d36ed27b42899145b6c
mari.7889 <mari.7889@mail.ru>
2024-01-27 17:49:02 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,116 @@
+const startBtn=document.querySelector('#start')
+const screens=document.querySelectorAll('.screen')
+const timeList=document.querySelector('#time-list')
+const timeEl=document.querySelector('#time')
+const board=document.querySelector('#board')
+let time=0
+let score=0
+startBtn.addEventListener('click', (event)=>{
+    event.preventDefault()
+    screens[0].classList.add('up')
+
+})
+
+timeList.addEventListener('click', event =>{
+    if (event.target.classList.contains('time-btn')){
+        time= parseInt(event.target.getAttribute('data-time'))
+        screens[1].classList.add('up')
+        startGame()
+
+    }
+})
+
+
+board.addEventListener('click', event =>{
+    
+    if(event.target.classList.contains('circle')){
+        score++
+        
+        event.target.remove()
+        createRandomCircle()
+        
+
+    }
+})
+
+
+function startGame (){
+    setInterval(decreaseTime, 1000)
+    createRandomCircle()
+    setTime(time)
+
+}
+
+
+function decreaseTime(){
+    if(time===0){
+        finishGame()
+
+    }else{
+        let current=--time
+    if(current<10){
+        current=`0${current}`
+    }
+    setTime(current)
+
+    }
+    
+
+}
+
+function setTime(value){
+    timeEl.innerHTML=`00:${value}`
+
+}
+
+function finishGame(){
+    timeEl.parentNode.remove()
+    board.innerHTML=`<h1>Счет: <span class="primary"> ${score} </span></h1>`
+
+}
+
+
+
+function createRandomCircle(){
+    
+    const circle=document.createElement('div')
+    const size=getRandomNumber(10, 80)
+    const {width, height}=board.getBoundingClientRect()
+    const x=getRandomNumber(0, width-size)
+    const y=getRandomNumber(0, height-size)
+    const color = getRandomColor();
+    circle.classList.add('circle')
+    circle.style.width=`${size}px`
+    circle.style.height=`${size}px`
+    circle.style.top=`${y}px`
+    circle.style.left=`${x}px`
+    circle.style.backgroundColor = color;
+
+    
+    
+    
+
+
+
+    board.append(circle)
+
+}
+
+
+function getRandomColor(){
+    
+    const randomNumber =() => Math.round(Math.random()*255);
+    const colors=[];
+    for (let i=0; i<3; i+=1){
+        const num=randomNumber();
+        colors.push(num);
+    }
+    return `rgb(${colors[0]},${colors[1]}, ${colors[2]})`;
+
+}
+
+
+
+function getRandomNumber(min,max){
+    return Math.round(Math.random()*(max-min)+min)
+}
